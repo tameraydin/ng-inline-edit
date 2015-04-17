ef65216cba49d11a3bd8eeca0ad6da3d9d3a6a90
Tamer Aydin <tamer.aydin@gmail.com>
2015-04-17 17:28:28 +0200

    fixed failing e2e test

--- test/specs/e2e/ng-inline-edit.js
+++ test/specs/e2e/ng-inline-edit.js
@@ -1,12 +1,13 @@
 describe('ng-inline-edit', function() {
 
   beforeEach(function() {
+    browser.ignoreSynchronization = true;
     browser.get('http://127.0.0.1:8080/demo/index.html');
   });
 
   // TODO: add more e2e tests
   it('should initialize properly', function() {
     var input = element.all(by.className('ng-inline-edit__input'));
-    expect(input.count()).toEqual(1);
+    expect(input.count()).toEqual(3);
   });
 });
