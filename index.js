a7a849ad4c62e691e1cd28d465a3ee4812f3ee11
Tamer Aydin <tamer.aydin@gmail.com>
2015-04-17 17:35:25 +0200

    improved demo page

--- index.js
+++ index.js
@@ -2,11 +2,49 @@
 
 angular
   .module('demoApp', [
+    'ngAnimate',
+    'ngMaterial',
     'angularInlineEdit'
   ])
-  .controller('demoMainController', function($scope) {
-    $scope.demoModel = 'An editable text';
-    $scope.demoChangeListener = function(newValue) {
-      alert(newValue);
-    };
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue')
+      .accentPalette('red');
+  })
+  .controller('demoMainController', function($scope, $timeout, $q) {
+    $scope.tabData = [{
+      selectedIndex: 0
+    }, {
+      selectedIndex: 0
+    }, {
+      selectedIndex: 0
+    }];
+
+    $scope.examples = [{
+      model: 'An editable text',
+      validator: function(newValue) {
+        return !!newValue;
+      }
+    }, {
+      model: 'Click here and delete me',
+      placeholder: 'Type your value here',
+      callback: function(newValue) {
+        console.log('value of your model is now: ' + newValue);
+      }
+    }, {
+      model: 'Need more than 10 chars',
+      validator: function(newValue) {
+        var defer = $q.defer();
+
+        $timeout(function() {
+          if (newValue.length > 10) {
+            defer.resolve();
+          } else {
+            defer.reject();
+          }
+        }, 2500);
+
+        return defer.promise;
+      }
+    }];
   });
