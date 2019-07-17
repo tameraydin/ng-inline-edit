7d063194672d4273ab4be2ba5bf57787ec5a42ba
Johan Dindaine <jojolapin972@gmail.com>
2019-07-17 07:05:25 +0100

    fixing travis issues and reverting the change to the local e2e file.

--- test/e2e.conf.js
+++ test/e2e.conf.js
@@ -2,6 +2,14 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['specs/e2e/ng-inline-edit.js'],
   capabilities: {
-    'browserName': 'firefox'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': [
+        'no-sandbox',
+        'no-default-browser-check',
+        'no-first-run',
+        'disable-default-apps'
+      ]
+    }
   }
 };
